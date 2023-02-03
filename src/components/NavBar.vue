17edb23a2e028239c84d77515d36811151bb0ff7
Zin Mg Nyunt <zinmgnyunt99@gmail.com>
2023-02-04 04:38:01 +0630

    add projec form and nav

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="nav-bar">
+    <router-link to="/">Project</router-link>
+    <router-link :to="{name:'AddProject'}">Add Project</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .nav-bar{
+        text-align: center;
+        margin: 40px 0;
+    }
+    .nav-bar a{
+        text-decoration: none;
+        display: inline-block;
+        padding: 10px;
+        color: #c4c4c4;
+        text-transform: uppercase;
+        font-weight: 600;
+    }
+    a.router-link-active{
+        border-bottom: 2px solid #00ffaa;
+    }
+</style>
---
+++ src/views/AddProjectView.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1>Add Project</h1>
+  <form>
+    <label>Project Title</label>
+    <input type="text">
+    <label>Project Detail</label>
+    <input type="text">
+    <button>Add Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  h1{
+    text-align: center;
+  }
+  form{
+    min-width: 500px;
+    margin-top: 4rem;
+  }
+  label{
+    display: block;
+    text-transform: uppercase;
+    font-weight: bold;
+    letter-spacing: .08rem;
+    color: #949494;
+    margin-bottom: .5rem;
+  }
+  input{
+    border: none;
+    padding: .5rem 0;
+    border-bottom: 1px solid #c4c4c4;
+    width: 100%;
+    margin-bottom: 1.5rem;
+    font-size: 1.5rem;
+  }
+  button{
+    display: block;
+    margin: 0 auto;
+    border-radius: 10px;
+    padding: 15px;
+    background-color: #00ffaa;
+    color: #f0f0f0;
+    border: none;
+    font-size: 1.1rem;
+    font-weight: 600;
+  }
+</style>
